67ea6574bf44bd301dde2cf167a657d16c297ade
乔晓啸 <91611485+xiaoioi@users.noreply.github.com>
2023-01-23 20:57:15 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-$img_array = glob('/images/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
-if(count($img_array) == 0) die('没找到图片文件。请先上传一些图片到 '.dirname(__FILE__).'/images/ 文件夹');
+$img_array = glob('/var/task/user/images/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
+if(count($img_array) == 0) die('没找到图片文件。请先上传一些图片到 '.dirname(__FILE__).'/images/');
 header('Content-Type: image/png');
 echo(file_get_contents($img_array[array_rand($img_array)]));
 ?>
